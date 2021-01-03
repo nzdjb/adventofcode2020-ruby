2d87b9ff3ba9e889014b6e71e964fa8f1ed9993a
Dave <dave@2a1b.com>
2021-01-03 00:07:56 +0000

    feat(gen): Assume we're going to want to read lines for most puzzles.

--- lib/util/template/lib/main.rb
+++ lib/util/template/lib/main.rb
@@ -6,10 +6,10 @@ require_relative '../../../util/puzzle/lib/puzzleutils'
 class MainYEARDAY
   include PuzzleUtils
   def self.part1(input)
-    input
+    PuzzleUtils.read_lines(input)
   end
 
   def self.part2(input)
-    input
+    PuzzleUtils.read_lines(input)
   end
 end
